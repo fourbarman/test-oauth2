9cdf0309c071f7ee9734b84746fe479547520e51
fourbarman <maks.java@yandex.ru>
2024-11-26 18:14:38 +0400

    Add sample delete method with role check and getting claims from jwt

--- src/main/java/ru/fourbarman/oauth2/spring/testoauth2/controller/TestController.java
+++ src/main/java/ru/fourbarman/oauth2/spring/testoauth2/controller/TestController.java
@@ -1,7 +1,10 @@
 package ru.fourbarman.oauth2.spring.testoauth2.controller;
 
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,9 +12,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test")
 public class TestController {
 
-    @GetMapping("/delete")
+    @GetMapping("/delete/{id}")
     @PreAuthorize("hasRole('admin')")
-    public String delete() {
+    public String delete(@PathVariable String id, @AuthenticationPrincipal Jwt jwt) {
+        System.out.println("jwt = " + jwt.getClaims());
+        System.out.println("id = " + id);
         return "delete work";
     }
 
--- src/main/java/ru/fourbarman/oauth2/spring/testoauth2/security/KCRoleConverter.java
+++ src/main/java/ru/fourbarman/oauth2/spring/testoauth2/security/KCRoleConverter.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Collection;
 import java.util.List;
 import java.util.Map;
-import java.util.stream.Collectors;
 
 /*
     Takes JWT payload and converts it to spring security roles
